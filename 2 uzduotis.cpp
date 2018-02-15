f5a18924cb9bd0b554a5bf772d387c05d4c4f40a
unknown <mariux454@gmail.com>
2018-02-15 10:36:47 +0200

    su mediana

--- 2 uzduotis.cpp
+++ 2 uzduotis.cpp
@@ -5,7 +5,7 @@
 int main ()
 {
     std::string vp;
-    int n=5,paz[100],egz,sum=0;
+    int n=6,paz[100],egz,sum=0,a;
     double galBalas;
     std::cout<<"iveskite mokinio/studento varda ir pavarde: ";
     getline (std::cin,vp);
@@ -16,11 +16,26 @@ int main ()
     }
     std::cout<<"iveskite mokinio/studento egzamino pazymi: ";
     std::cin>>egz;
+    std::cout>>endl;
     for (int i=0; i<n; i++)
+        for (int j=i; j<n; j++)
+        {
+            a=0;
+            if (paz[j]<paz[i])
+                {
+                    a=paz[i];
+                    paz[i]=paz[j];
+                    paz[j]=a;
+                }
+        }
+    if (n%2!=0)
+        galBalas=0.4*paz[n/2]+0.6*egz;
+    else
     {
-        sum+=paz[i];
+        double med=(paz[n/2-1]+paz[n/2])/2;
+        galBalas=0.4*med+0.6*egz;
     }
-    galBalas=0.4*(sum/n)+0.6*egz;
+
     std::cout<<"moknio/studento galutinis balas: "<<std::fixed<<std::setprecision(2)<<galBalas;
     return 0;
 }
