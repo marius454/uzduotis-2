75da30647c070485db8f56450254e63fb1ee449a
unknown <mariux454@gmail.com>
2018-02-15 12:15:05 +0200

    mediana, vektoriai, pasirenkamas n

--- 2 uzduotis.cpp
+++ 2 uzduotis.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include <cmath>
 #include <iomanip>
-
+#include <vector>
 int main ()
 {
     std::string vp;
-    int n,egz,sum=0,a;
+    int n,egz,sum=0,a,b;
     double galBalas;
     std::cout<<"iveskite mokinio/studento varda ir pavarde: ";
     getline (std::cin,vp);
     std::cout<<"iveskite kiek pazymiu bus vedama: "; std::cin>>n;
-    int paz[n];
+    std::vector<int> paz;
     std::cout<<"iveskite visus "<<n<<" namu darbu pazymius: "<<std::endl;
     for (int i=0;i<n;i++)
     {
-        std::cin>>paz[i];
+        std::cin>>b;
+        paz.push_back(b);
     }
     std::cout<<"iveskite mokinio/studento egzamino pazymi: ";
     std::cin>>egz;
@@ -23,18 +24,18 @@ int main ()
         for (int j=i; j<n; j++)
         {
             a=0;
-            if (paz[j]<paz[i])
+            if (paz.at(j)<paz.at(i))
                 {
-                    a=paz[i];
-                    paz[i]=paz[j];
-                    paz[j]=a;
+                    a=paz.at(i);
+                    paz.at(i)=paz.at(j);
+                    paz.at(j)=a;
                 }
         }
     if (n%2!=0)
-        galBalas=0.4*paz[n/2]+0.6*egz;
+        galBalas=0.4*paz.at(n/2)+0.6*egz;
     else
     {
-        double med=(paz[n/2-1]+paz[n/2])/2;
+        double med=(paz.at(n/2-1)+paz.at(n/2))/2;
         galBalas=0.4*med+0.6*egz;
     }
 
