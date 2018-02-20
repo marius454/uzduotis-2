b9c323b390733f36d07aea15b43a0f89180bec94
unknown <mariux454@gmail.com>
2018-02-20 13:37:30 +0200

    random pazymiai

--- 2 uzduotis.cpp
+++ 2 uzduotis.cpp
@@ -1,55 +1,49 @@
 #include <iostream>
+#include <cmath>
 #include <iomanip>
 #include <vector>
-#include <string>
-
 int main()
 {
 	std::string vp;
-	int n = 0, egz, a, max = 1;
+	int n = 9, egz, a, b = 0;
 	double galBalas;
-	int* paz = new int[max];
+	std::vector<int> paz;
 	std::cout << "iveskite mokinio/studento varda ir pavarde: ";
-	std::getline(std::cin, vp);
+	getline(std::cin, vp);
 
-	std::cout << "iveskite visus namu darbu pazymius: " << std::endl;
-	while (std::cin >> paz[n] && paz[n] != -1)
+	std::random_device rd;
+	std::mt19937 mt(rd());
+	std::uniform_int_distribution<int> dist(0, 10);
+	for (int i = 0; i<n; i++)
 	{
-		n++;
-		if (n >= max)
-		{
-			max = max * 2;
-			int* temp = new int[max];
-			for (int i = 0; i < n; i++)
-			{
-				temp[i] = paz[i];
-			}
-			delete[] paz;
-			paz = temp;
-		}
+		std::cout << dist(mt) << " ";
+		paz.push_back(dist(mt));
 	}
+	std::cout << std::endl;
+
 	std::cout << "iveskite mokinio/studento egzamino pazymi: ";
 	std::cin >> egz;
 	std::cout << std::endl;
-	for (unsigned int i = 0; i < n; i++)
-		for (unsigned int j = i; j < n; j++)
+	for (unsigned int i = 0; i<paz.size(); i++)
+		for (unsigned int j = i; j<paz.size(); j++)
 		{
 			a = 0;
-			if (paz[j] < paz[i])
+			if (paz.at(j)<paz.at(i))
 			{
-				a = paz[i];
-				paz[i] = paz[j];
-				paz[j] = a;
+				a = paz.at(i);
+				paz.at(i) = paz.at(j);
+				paz.at(j) = a;
 			}
 		}
-	if ((n - 1) % 2 != 0)
-		galBalas = 0.4*paz[(n / 2) + 1] + 0.6*egz;
+	if ((paz.size() - 1) % 2 != 0)
+		galBalas = 0.4*paz.at((paz.size()) / 2) + 0.6*egz;
 	else
 	{
-		double med = (paz[(n) / 2] + paz[(n) / 2 + 1]) / 2;
+		double med = (paz.at((paz.size()) / 2 - 1) + paz.at((paz.size()) / 2)) / 2;
 		galBalas = 0.4*med + 0.6*egz;
 	}
 
 	std::cout << "moknio/studento galutinis balas: " << std::fixed << std::setprecision(2) << galBalas << std::endl;
 	return 0;
 }
+
