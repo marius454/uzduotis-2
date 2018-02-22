be0cd1de0e51b641fb1ebed99b588d9506f4ecdb
unknown <mariux454@gmail.com>
2018-02-22 12:51:30 +0200

    v0.2

--- 2 uzduotis.cpp
+++ 2 uzduotis.cpp
@@ -1,50 +1,110 @@
 #include <iostream>
-#include <cmath>
 #include <iomanip>
+#include <fstream>
 #include <vector>
-#include <random>
+#include <string>
+#include <algorithm>
+
+struct studentai
+{
+	std::string vardas, pavarde;
+	std::vector<int> paz;
+	unsigned short int egz;
+	double vid;
+	double med;
+};
+
 int main()
 {
-	std::string vp;
-	int n = 9, egz, a, b = 0;
-	double galBalas;
-	std::vector<int> paz;
-	std::cout << "iveskite mokinio/studento varda ir pavarde: ";
-	getline(std::cin, vp);
-
-	std::random_device rd;
-	std::mt19937 mt(rd());
-	std::uniform_int_distribution<int> dist(0, 10);
-	for (int i = 0; i<n; i++)
+	unsigned int m, nd, nr;
+	std::ifstream fd("kursiokai.txt");
+	std::ofstream fr("rezultatai.txt");
+	studentai stud[100];
+	std::cout << "pasirinkite:" << std::endl;
+	std::cout << "1) ivesti duomenis is klaviaturos" << std::endl;
+	std::cout << "2) nuskaityti duomenys is failo (kursiokai.txt)" << std::endl;
+	std::cin >> nr;
+	if (nr == 1)
 	{
-		std::cout << dist(mt) << " ";
-		paz.push_back(dist(mt));
-	}
-	std::cout << std::endl;
-
-	std::cout << "iveskite mokinio/studento egzamino pazymi: ";
-	std::cin >> egz;
-	std::cout << std::endl;
-	for (unsigned int i = 0; i<paz.size(); i++)
-		for (unsigned int j = i; j<paz.size(); j++)
+		std::cout << "iveskite kiek bus vedama mokiniu: ";
+		std::cin >> m;
+		std::cout << "iveskite kiek bus vedama nd pazymiu: ";
+		std::cin >> nd;
+		for (unsigned int i = 0; i < m; i++)
 		{
-			a = 0;
-			if (paz.at(j)<paz.at(i))
+			std::cout << "iveskite " << i << "-ojo mokinio(es) pavarde: ";
+			std::cin >> stud[i].pavarde;
+			std::cout << "iveskite " << i << "-ojo mokinio(es) varda: ";
+			std::cin >> stud[i].vardas;
+			std::cout << "iveskite " << i << "-ojo mokinio(es) " << nd << " namu darbu pazymius:" << std::endl;
+			for (unsigned int j = 0; j < nd; j++)
 			{
-				a = paz.at(i);
-				paz.at(i) = paz.at(j);
-				paz.at(j) = a;
+				int a;
+				std::cin >> a;
+				stud[i].paz.push_back(a);
 			}
+			std::cout << "iveskite " << i << "-ojo mokinio egzamino bala: ";
+			std::cin >> stud[i].egz;
 		}
-	if ((paz.size() - 1) % 2 != 0)
-		galBalas = 0.4*paz.at((paz.size()) / 2) + 0.6*egz;
+	}
 	else
 	{
-		double med = (paz.at((paz.size()) / 2 - 1) + paz.at((paz.size()) / 2)) / 2;
-		galBalas = 0.4*med + 0.6*egz;
+		fd >> m >> nd;
+		for (unsigned int i = 0; i < m; i++)
+		{
+			fd >> stud[i].pavarde;
+			fd >> stud[i].vardas;
+			for (unsigned int j = 0; j < nd; j++)
+			{
+				int a;
+				fd >> a;
+				stud[i].paz.push_back(a);
+			}
+			fd >> stud[i].egz;
+		}
 	}
+	for (unsigned int i = 0; i < m; i++)
+	{
+		int sum = 0;
+		for (unsigned int j = 0; j < nd; j++)
+		{
+			sum = sum + stud[i].paz.at(j);
+		}
+		stud[i].vid = 0.4*(sum / nd) + 0.6*stud[i].egz;
 
-	std::cout << "moknio/studento galutinis balas: " << std::fixed << std::setprecision(2) << galBalas << std::endl;
+	}
+	for (unsigned int i = 0; i < m; i++)
+
+		std::sort(stud[i].paz.begin(), stud[i].paz.end());
+
+	for (unsigned int i = 0; i < m; i++)
+	{
+		if ((nd) % 2 != 0)
+			stud[i].med = 0.4*stud[i].paz[nd / 2] + 0.6*stud[i].egz;
+		else
+		{
+			double med = (stud[i].paz[nd / 2 - 1] + stud[i].paz[nd / 2]) / 2;
+			stud[i].med = 0.4*med + 0.6*stud[i].egz;
+		}
+	}
+	for (unsigned int i = 0; i < m; i++)
+		for (unsigned int j = i; j < m; j++)
+		{
+			studentai temp;
+			if (stud[i].pavarde > stud[i].vardas)
+			{
+				temp = stud[i];
+				stud[i] = stud[j];
+				stud[j] = temp;
+			}
+		}
+	fr << std::setw(20) << std::left << "Pavarde" << std::setw(20) << std::left << "Vardas" << std::setw(30) << "Galutinis-vidurkis" << "Galutinis-mediana" << std::endl;
+	for (unsigned int i = 0; i < m; i++)
+	{
+		fr << std::setw(20) << std::left << stud[i].pavarde << std::setw(20) << std::left << stud[i].vardas << std::setw(30);
+		fr << std::fixed << std::setprecision(2) << stud[i].vid << stud[i].med << std::endl;
+	}
+	fd.close();
+	fr.close();
 	return 0;
 }
-
